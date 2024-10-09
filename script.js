153ecb998d75e51288ad09858db176f2095ce49f
nirb <nir.bejerano@gmail.com>
2024-10-09 14:54:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ function fetchAndDisplayText(url, elementId) {
 }
 
 // Replace with the actual URL of your public file
-const text1_url = "https://drive.google.com/file/d/1eOGWICX7pLxCdQmo9mOWl6511AuB_vZ6/view?usp=sharing";
-const text2_url = "https://drive.google.com/file/d/1NsIcc3N1dElS6hZ97qA-TAHzGeoJfJBS/view?usp=sharing";
-const text3_url = "https://drive.google.com/file/d/1G6fxdJOG_SVv1EQECQrlvmvZnI9Swj1G/view?usp=sharing";
-const text4_url = "https://drive.google.com/file/d/1W-bV2wjowcCqtkTUKfHH2He0ZNYKf8Ta/view?usp=sharing";
+const text1_url = "https://tv-info.s3.amazonaws.com/text1.txt";
+const text2_url = "https://tv-info.s3.amazonaws.com/text1.txt";
+const text3_url = "https://tv-info.s3.amazonaws.com/text1.txt";
+const text4_url = "https://tv-info.s3.amazonaws.com/text1.txt";
 
 fetchAndDisplayText(text1_url, "object1");
 fetchAndDisplayText(text2_url, "object2");
